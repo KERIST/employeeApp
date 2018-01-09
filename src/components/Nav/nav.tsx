fe3590e18b7a193f91b41f30ef54fca6c72d1491
Alexander Nikiforov <alexandernanotexno@gmail.com>
2018-01-09 17:01:16 +0300

    'stable'

--- src/components/Nav/nav.tsx
+++ src/components/Nav/nav.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 import NavItem from './nav-item';
 import SearchContainer from '../../containers/SearchContainer';
+import sideMenu from 'src/reducers/sideMenu';
 
 export interface PropsType {
     items: Array<{
@@ -12,31 +13,24 @@ export interface PropsType {
     toggleVisible: () => void;
 }
 
-// const Nav = (props: PropsType) => {
-//     const { items } = props;
-//     return <section className={props.visible ? 'side-menu side-menu_visible' : 'side-menu'}>
-//         <div className='side-menu__container ui vertical inverted menu top input inverted' style={{ borderRadius: 0 }}>
-//             <SearchContainer />
-//             {items.map((item) => (<NavItem key={item.itemHeader} itemHeader={item.itemHeader} navItems={item.navItems} />))}
-//         </div>
-//     </section>
-// }
+const Nav = (props: PropsType) => {
+    const { items } = props;
+    return <section className={props.visible ? 'side-menu side-menu_visible' : 'side-menu'}>
+        <div className='side-menu__container ui vertical inverted menu top input inverted' style={{ borderRadius: 0 }}>
+            <SearchContainer />
+            {items.map((item) => (<NavItem key={item.itemHeader} itemHeader={item.itemHeader} navItems={item.navItems} />))}
+        </div>
+    </section>
+}
 
 class SideMenu extends React.Component<any, any> {
-    constructor(props: any) {
+    constructor (props: any){
         super(props);
-    }
-
-    handleSectionClick(e: any) {
-        const target = e.target;
-        if(target.tagName === 'A' || ~target.className.indexOf('side-menu_visible')){
-            this.props.toggleVisible();
-        }
     }
 
     render() {
         const {visible, items} = this.props;
-        return <section className={visible ? 'side-menu side-menu_visible' : 'side-menu'} onClick={(e: any) => {this.handleSectionClick(e)}}>
+        return <section className={visible ? 'side-menu side-menu_visible' : 'side-menu'}>
         <div className='side-menu__container ui vertical inverted menu top input inverted' style={{ borderRadius: 0 }}>
             <SearchContainer />
             {items.map((item: any) => (<NavItem key={item.itemHeader} itemHeader={item.itemHeader} navItems={item.navItems} />))}
@@ -45,4 +39,4 @@ class SideMenu extends React.Component<any, any> {
     }
 }
 
-export default SideMenu;
+export default Nav;
