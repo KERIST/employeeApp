efa61eda247d6f00b4a2c80b8b1ba103e0492d4e
Alexander Nikiforov <alexandernanotexno@gmail.com>
2018-01-10 11:23:32 +0300

    'stable

--- src/components/Nav/nav.tsx
+++ src/components/Nav/nav.tsx
@@ -13,30 +13,29 @@ export interface PropsType {
     toggleVisible: () => void;
 }
 
-const Nav = (props: PropsType) => {
-    const { items } = props;
-    return <section className={props.visible ? 'side-menu side-menu_visible' : 'side-menu'}>
-        <div className='side-menu__container ui vertical inverted menu top input inverted' style={{ borderRadius: 0 }}>
-            <SearchContainer />
-            {items.map((item) => (<NavItem key={item.itemHeader} itemHeader={item.itemHeader} navItems={item.navItems} />))}
-        </div>
-    </section>
-}
-
 class SideMenu extends React.Component<any, any> {
-    constructor (props: any){
+    constructor(props: any) {
         super(props);
     }
 
+    handleSectionClick(e: any) {
+        const target = e.target;
+        
+        if (target.tagName === 'A' || ~target.className.indexOf('side-menu_visible')) {
+            console.log('hello');
+            this.props.toggleVisible();
+        }
+    }
+
     render() {
-        const {visible, items} = this.props;
-        return <section className={visible ? 'side-menu side-menu_visible' : 'side-menu'}>
-        <div className='side-menu__container ui vertical inverted menu top input inverted' style={{ borderRadius: 0 }}>
-            <SearchContainer />
-            {items.map((item: any) => (<NavItem key={item.itemHeader} itemHeader={item.itemHeader} navItems={item.navItems} />))}
-        </div>
-    </section>
+        const { visible, items } = this.props;
+        return <section className={visible ? 'side-menu side-menu_visible' : 'side-menu'} onClick={(e: any) => {this.handleSectionClick(e)}}>
+            <div className='side-menu__container ui vertical inverted menu top input inverted' style={{ borderRadius: 0 }}>
+                <SearchContainer />
+                {items.map((item: any) => (<NavItem key={item.itemHeader} itemHeader={item.itemHeader} navItems={item.navItems} />))}
+            </div>
+        </section>
     }
 }
 
-export default Nav;
+export default SideMenu;
